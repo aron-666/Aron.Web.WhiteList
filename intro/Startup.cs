23e48e2f4071d838e0a82ba0409d00cc02ed523d
aron-666 <aronkao0115@gmail.com>
2020-11-15 17:29:37 +0800

    add remarks

--- intro/Startup.cs
+++ intro/Startup.cs
@@ -31,7 +31,7 @@ namespace intro
         }
 
         public IConfiguration Configuration { get; }
-
+ 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -66,8 +66,70 @@ namespace intro
             services.AddSingleton<IContentChecker, ContentChecker>();
 
             //2. inject IWhiteListService
-            services.AddSingleton<IWhiteListService, MyWhiteListService>();
 
+            ///inject WhiteListService from database intro.Models.WhiteLists.WhiteListContext
+            //services.AddSingleton<IWhiteListService, MyWhiteListService>();
+
+            //or inject WhiteListService from hard-coding
+#region hard-coding
+            //inject whitelist record
+            {
+                var whitelists = new List<WhiteLists>()
+                {
+                    new WhiteLists(){
+                        Id = 1,
+                        Name = "register",
+                        Route = "/Identity/Account/Register",
+                        WlContent = new List<WlContent>()
+                    }
+                };
+                var content = new List<WlContent>()
+                {
+                    //Allow ::1 (localhost)
+                    new WlContent()
+                    {
+                        Id = 1,
+                        Wid = whitelists.First().Id,
+                        Policy = "Allow",
+                        Content = "::1",
+                        Source = whitelists.First()
+                    },
+                    //Allow 127.0.0.1
+                    new WlContent()
+                    {
+                        Id = 2,
+                        Wid = whitelists.First().Id,
+                        Policy = "Allow",
+                        Content = "127.0.0.1",
+                        Source = whitelists.First()
+                    },
+                    //Allow 192.168.64.129-254
+                    new WlContent()
+                    {
+                        Id = 3,
+                        Wid = whitelists.First().Id,
+                        Policy = "Allow",
+                        Content = "192.168.64.128/25",
+                        Source = whitelists.First()
+                    },
+                    //Deny 192.168.64.201
+                    new WlContent()
+                    {
+                        Id = 4,
+                        Wid = whitelists.First().Id,
+                        Policy = "Deny",
+                        Content = "192.168.64.201",
+                        Source = whitelists.First()
+                    },
+                };
+                whitelists[0].WlContent = content;
+
+                
+                services.AddSingleton<IEnumerable<WhiteLists>>(whitelists);
+            }
+            services.AddSingleton<IWhiteListService, WhiteListService>();
+
+#endregion hard-coding
             //3. inject options. if not need, you can not inject. 
             services.AddSingleton(whiteListOptions);
 
